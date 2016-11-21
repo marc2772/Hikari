1ad48c43b9e5d146ffa3e6adf94104c827c3e46c
marc2772 <marc_2772@hotmail.com>
2016-11-21 14:10:32 -0500

    Added fade out (music+camera) in level01

--- Project/Assets/Scripts/NextLevel.cs
+++ Project/Assets/Scripts/NextLevel.cs
@@ -1,12 +1,38 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NextLevel : MonoBehaviour
 {
+	public AudioSource backgroundMusic;
+	public Image blackImage;
+
+	void Awake()
+	{
+		blackImage.rectTransform.localScale = new Vector2(Screen.width, Screen.height);
+	}
 
 	void OnTriggerEnter(Collider col)
 	{
+		StartCoroutine(fadeOut());
+	}
+
+	IEnumerator fadeOut()
+	{
+		float volume = backgroundMusic.volume;
+		while(volume > 0.1f)
+		{
+			volume -= Time.deltaTime;
+			backgroundMusic.volume = volume;
+
+			Color color = blackImage.color;
+			color.a += Time.deltaTime;
+			blackImage.color = color;
+
+			yield return new WaitForSeconds(0.08f);
+		}
+		backgroundMusic.volume = 0.0f;
 		SceneManager.LoadScene("Level02");
 	}
 }

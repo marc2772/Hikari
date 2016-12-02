0e7e221fa1d59af0cc379b10b419c857600da974
Cyril-W <cwirtz@ensc.fr>
2016-12-02 18:12:56 -0500

    Burning treeeeees !

---
+++ Project/Assets/Scripts/BurnOnPassage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurnOnPassage : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.z >= transform.position.z) {
+			GetComponent<ParticleSystem> ().Play ();
+			this.enabled = false;
+		}
+	}
+}

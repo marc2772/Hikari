ee3390b92f3b6c4146473a148b5f3634623312c9
marc2772 <marc_2772@hotmail.com>
2016-12-11 11:21:26 -0500

    Fixed respawning when dying in wind

--- Project/Assets/Scripts/RespawnAll.cs
+++ Project/Assets/Scripts/RespawnAll.cs
@@ -5,12 +5,16 @@ using System.Collections.Generic;
 public class RespawnAll : MonoBehaviour
 {
 	public List<AddHealth> branches;
+	public List<WindPush> windPush;
 
 	public void Respawn()
 	{
+		
 		PlayerStateManager.Instance.Respawn();
 
 		foreach(AddHealth branch in branches)
 			branch.Respawn();
+		foreach(WindPush w in windPush)
+			w.StopTimer();
 	}
 }

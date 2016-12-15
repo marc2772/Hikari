a135a67cdb4466b59a674dcc935ff169a7cbaccf
marc2772 <marc_2772@hotmail.com>
2016-12-15 15:24:43 -0500

    Added cinematic scene

---
+++ Project/Assets/Scripts/Video.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Video : MonoBehaviour
+{
+    public MovieTexture movie;
+
+	Renderer ren;
+	AudioSource a;
+
+	void Awake()
+	{
+		ren = GetComponent<Renderer>();
+		a = GetComponent<AudioSource>();
+	}
+
+	void Start()
+	{
+        ren.material.mainTexture = movie as MovieTexture;
+        a.clip = movie.audioClip;
+
+        movie.Play() ;
+        a.Play();
+    }
+    
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(0) || !movie.isPlaying)
+			End();
+	}
+
+	void End()
+	{
+		movie.Stop();
+		SceneManager.LoadScene("Menu");
+	}
+	
+}
